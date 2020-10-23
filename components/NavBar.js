c7ab2fa04a668d637666a26dfd27d3770b7f81da
Nauhl Valdez <Nauhl.ValdezBenavides@gmail.com>
2020-10-23 00:24:01 -0500

    working on footer

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,77 +1,60 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
-//import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export default function NavBar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <img src="../utils/img/home_inventory.png" className="img-fluid" width="100px" height="100px" alt=""></img>
-        <a className="navbar-brand" href="/">Inventory</a>
+        <a className="navbar-brand text-light" href="/"><i className="fas fa-home"></i> &nbsp; Inventory</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/items">Items</a>
-            </li>
+              <a className="nav-link text-light" href="/items"><i className="fas fa-box"></i> &nbsp; Items</a>
+            </li> 
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 More
         </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="/categories">Categories</a>
+                <a className="dropdown-item" href="/categories"><i className="fas fa-window-restore"></i> &nbsp; Categories</a>
                 <>
-                  <a className="dropdown-item" href="/conditions">Condition</a>
-                  <a className="dropdown-item" href="/locations">Locations</a>
+                  <a className="dropdown-item" href="/conditions"><i className="far fa-calendar-check"></i> &nbsp; Condition</a>
+                  <a className="dropdown-item" href="/locations"><i className="far fa-compass"></i> &nbsp; Locations</a>
                 </>
-                <a className="dropdown-item" href="/rooms">Rooms</a>
+                <a className="dropdown-item" href="/rooms"><i className="far fa-list-alt"></i> &nbsp; Rooms</a>
 
               </div>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">About us</a>
+              <a className="nav-link text-light" href="/about"><i className="fas fa-user-astronaut"></i> &nbsp; About us</a>
             </li>
           </ul>
         </div>
       </nav>
 
-      { /* FOOTER */}
+      { /* FOOTER */} 
       <div className="container">
-      <footer className="page-footer bg-blue">
-        <div className="bg-success">
-          <div className="container">
-            <div className="row py-4 d-flex align-items-center">
+        <footer className="page-footer bg-blue" id="Footer" >
+          <div className="bg-success">
+            <div className="container">
+              <div className="row py-4 d-flex align-items-center">
 
-              <div className="col-md-12 text-center">
-
-                <a href="#"><i className="fab fa-github text-white mr-4"></i></a>
-                <a href="#"><i className="fab fa-twitter-square text-white mr-4"></i></a>
+                <div className="col-md-12 text-center">
+                <h4 className="text-center text-light" >Inventory </h4> 
+                <a className="text-center text-light">Designed by me</a> &nbsp;
+                  <a href="https://github.com/Nauhl"><i className="fab fa-github text-white mr-4"></i></a>
+                  <a href="#"><i className="fab fa-twitter-square text-white mr-4"></i></a>
+                  <a className="text-right text-light">@ Vivenda 2020 </a>
+                </div>
 
               </div>
-
             </div>
           </div>
-        </div>
-      </footer>
-
-      <div className="container text-center text-md-left mt-5">
-        <div className="row">
-          <div className="col-md-3 mx-auto mb-4">
-            <h6 className="text-uppercase font-weight-bold">Inventory</h6>
-            <hr className="bg-success mb-4 mt-0 d-inline-block mx-auto" ></hr>
-            <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipiscing elit leo duis tempus pellentesque platea dictumst, bibendum hendrerit suspendisse quisque consequat nulla erat suscipit tempor sapien nec. Vestibulum quam arcu posuere hendrerit maecenas et, placerat parturient eu mattis curae sagittis facilisis, a netus ridiculus ante ad. Urna aliquam sem mauris pellentesque nam neque vitae, ultrices sagittis molestie egestas et interdum viverra quisque, aliquet curae auctor mattis semper augue.
-
-Suscipit sociosqu dictumst ac penatibus conubia eros leo donec rhoncus porttitor, magna sapien vitae tincidunt sagittis feugiat sodales curae tellus, aptent cum dictum proin netus non habitasse rutrum fusce. In justo risus sem et sagittis pharetra euismod suspendisse vestibulum aenean platea accumsan felis, phasellus cum vitae gravida netus tristique erat ultrices taciti ridiculus ac. Vivamus imperdiet fringilla ad id cum non lacus inceptos euismod, metus placerat pulvinar magnis dapibus rhoncus suspendisse.</p>
-          </div>
-          
-          <div className="col-md-3 mx-auto mb-4">
-            <h6 className="text-uppercase font-weight-bold">Vivendo @ 2020</h6>
-            <hr className="bg-success mb-4 mt-0 d-inline-block mx-auto" ></hr>
-        </div>
-      </div>
-      </div>
+        </footer>
+        
       </div>
     </div>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Head from "next/Head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../../components/NavBar";
-
+import Footer  from '../../styles/estilos.css';
 
 export default function MyApp() {
 
@@ -13,18 +13,20 @@ export default function MyApp() {
       <Head>
         <title>Inventory</title>
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        
       </Head>
-            <NavBar />
+
+      <NavBar />
 
     </>
   )
